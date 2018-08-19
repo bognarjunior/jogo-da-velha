2b6f0d214306a59c1e6f33ef274c892992fc355b
Bognar Junior <bognar_junior@yahoo.com.br>
2018-08-19 12:19:00 -0300

    Criando o modulo e componente
    Criando o modulo e o componente de jogo da velha

---
+++ src/app/jogo-da-velha/jogo-da-velha.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { JogoDaVelhaComponent } from './jogo-da-velha.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    JogoDaVelhaComponent
+  ],
+  exports: [
+    JogoDaVelhaComponent
+  ]
+})
+export class JogoDaVelhaModule { }
